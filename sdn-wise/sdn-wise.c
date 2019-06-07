78c72d704967d081acd068e5c76ac3b22c35db27
Unknown <tobias.kasper@hotmail.com>
2019-06-07 11:59:29 +0200

    print new line

--- sdn-wise/sdn-wise.c
+++ sdn-wise/sdn-wise.c
@@ -301,6 +301,7 @@ static const uint8_t destinations[NETWORK_SIZE] = { 22, 23, 39, 34, 4, 37, 13, 3
         PRINTF("UART_RECEIVE_EVENT:");
         packet_t* p = (packet_t*)data;
         print_packet(p);
+        PRINTF("\n");
         process_post(&packet_handler_proc, NEW_PACKET_EVENT, (process_data_t)data);
         break;
         case RF_B_RECEIVE_EVENT:
